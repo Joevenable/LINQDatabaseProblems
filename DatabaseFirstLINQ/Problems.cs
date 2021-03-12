5415a02f3fdb693881413c06c180a2e97ced4610
Denis Zavertyayev <denisz2277@gmail.com>
2021-03-12 12:18:09 -0800

    Completed the logic for Problem8 to utilize include() and where() to find the products in a shopping cart that belongs to an email.

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -22,7 +22,7 @@ namespace DatabaseFirstLINQ
             //ProblemFive();
             //ProblemSix();
             //ProblemSeven();
-            //ProblemEight();
+            ProblemEight();
             //ProblemNine();
             //ProblemTen();
             //ProblemEleven();
@@ -92,7 +92,7 @@ namespace DatabaseFirstLINQ
             // Then print each user's email and registration date to the console.
             foreach (User user in userReg)
             {
-                Console.WriteLine($"{user.Email} {user.RegistrationDate}");
+                Console.WriteLine($"Email: {user.Email} Registration Date: {user.RegistrationDate}");
             }
         }
 
@@ -103,7 +103,7 @@ namespace DatabaseFirstLINQ
             // Then print each user's email and registration date to the console.
             foreach(User user in userReg)
             {
-                Console.WriteLine($"{user.Email} {user.RegistrationDate}");
+                Console.WriteLine($"Email: {user.Email} Registration Date: {user.RegistrationDate}");
             }
         }
 
@@ -123,7 +123,12 @@ namespace DatabaseFirstLINQ
         private void ProblemEight()
         {
             // Write a LINQ query that retreives all of the products in the shopping cart of the user who has the email "afton@gmail.com".
+            var products = _context.ShoppingCarts.Include(p => p.Product).Where(u => u.User.Email == "afton@gmail.com");      //q shopping carts and include product  -- use a .where function to filter
             // Then print the product's name, price, and quantity to the console.
+            foreach (ShoppingCart product in products)
+            {
+                Console.WriteLine($"Product Name: {product.Product.Name} Price: {product.Product.Price} Quantity: {product.Quantity}");
+            }
 
         }
 
