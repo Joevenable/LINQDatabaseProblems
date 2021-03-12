cb130c580608f1519884168349c562db23fb3a37
Joevenable <binechorighteous@gmail.com>
2021-03-12 11:08:38 -0600

    Solved problem 3 using .Where to add a condition and setting the condition to have price > 150

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -61,7 +61,13 @@ namespace DatabaseFirstLINQ
         private void ProblemThree()
         {
             // Write a LINQ query that gets each product where the products price is greater than $150.
+            var products = _context.Products.Where(p => p.Price > 150);
             // Then print the name and price of each product from the above query to the console.
+            foreach (Product product in products)
+            {
+                Console.WriteLine(products);
+            }
+            
 
         }
 
